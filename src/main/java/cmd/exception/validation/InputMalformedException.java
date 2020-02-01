fb8416dc7351b8cdaeeef2c9bc1eed5d67513c72
Kyran Rana <kyranportfolio@gmail.com>
2020-02-01 12:05:09 +0000

    Update InputMalformedException.java

--- src/main/java/cmd/exception/validation/InputMalformedException.java
+++ src/main/java/cmd/exception/validation/InputMalformedException.java
@@ -1,10 +1,5 @@
 package cmd.exception.validation;
 
-/**
- * Input validation exception thrown when input is malformed.
- *
- * @author kyranrana
- */
 public class InputMalformedException extends RuntimeException {
   public InputMalformedException(String message) {
     super(message);
